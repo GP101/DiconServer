a7ee48b70a79fb5a82e466320e6e2d9594352874
jintaeks <jintaeks@gmail.com>
2024-10-23 16:15:23 +0900

    boost serialization library was replaced with cereal(open source serialization library).

--- MyServer Steps/MyServer Step01 Basic Components/Common/KPacket.h
+++ MyServer Steps/MyServer Step01 Basic Components/Common/KPacket.h
@@ -1,9 +1,7 @@
 #pragma once
-#include <boost/serialization/vector.hpp>
-#include <boost/serialization/string.hpp>
-#include <boost/serialization/version.hpp>
-#include <boost/archive/text_oarchive.hpp>
-#include <boost/archive/text_iarchive.hpp>
+#include <cereal/archives/binary.hpp>
+#include <cereal/types/string.hpp>
+#include <cereal/types/vector.hpp>
 #include <memory>
 #include <sstream>
 #include "NetUtil.h"
@@ -48,14 +46,6 @@ public:
     template<typename T>
     void                SetData( unsigned short usPacketId, const T& data );
 
-    template<typename Archive>
-    void                serialize( Archive& ar, const unsigned int version )
-                        {
-                            ar & m_nSenderUid;
-                            ar & m_usPacketId;
-                            ar & m_buffer;
-                        }//serialize()
-
 public:
     LONGLONG            m_nSenderUid;
     unsigned short      m_usPacketId;
@@ -72,14 +62,21 @@ void KPacket::SetData( unsigned short usPacketId, const T& data_ )
     m_usPacketId = usPacketId;
 
     std::stringstream   ss;
-    boost::archive::text_oarchive oa{ ss };
+    cereal::BinaryOutputArchive oa(ss); // Create an output archive
     oa << data_;
 
     std::string& str = ss.str();
-    m_buffer.reserve( str.size() );
-    m_buffer.assign( str.begin(), str.end() );
+    m_buffer.reserve(str.size());
+    m_buffer.assign(str.begin(), str.end());
 }//KPacket::SetData()
 
+template <typename Archive>
+void serialize(Archive& ar, KPacket& a, const unsigned int version)
+{
+    ar& a.m_nSenderUid;
+    ar& a.m_usPacketId;
+    ar& a.m_buffer;
+}//serialize()
 
 template<typename T>
 bool BufferToPacket( IN std::vector<char>& buffer, OUT T& data )
@@ -90,12 +87,12 @@ bool BufferToPacket( IN std::vector<char>& buffer, OUT T& data )
     // alternative (slow) implementation. jintaeks on 2017-08-24_20-08
     //std::stringstream ss;
     //std::copy(buffer.begin(), buffer.end(), std::ostream_iterator<char>(ss));
-    //boost::archive::text_iarchive ia{ ss };
+    //cereal::BinaryInputArchive ia(ss);
     //ia >> data;
 
-    vectorwrapbuf<char> databuf( buffer );
-    std::istream is( &databuf );
-    boost::archive::text_iarchive ia{ is };
+    vectorwrapbuf<char> databuf(buffer);
+    std::istream is(&databuf);
+    cereal::BinaryInputArchive ia(is);
     ia >> data;
     return true;
 }//BufferToPacket()
@@ -104,7 +101,7 @@ bool BufferToPacket( IN std::vector<char>& buffer, OUT T& data )
 template<typename T>
 void BufferToPacket( IN std::stringstream& ss_, OUT T& packet_ )
 {
-    boost::archive::text_iarchive ia{ ss_ };
+    cereal::BinaryInputArchive ia(ss_);
     ia >> packet_;
 }//BufferToPacket()
 
@@ -113,20 +110,20 @@ template<typename T>
 void PacketToBuffer( IN T& packet_, OUT std::vector<char>& buffer_ )
 {
     std::stringstream   ss;
-    boost::archive::text_oarchive oa{ ss };
+    cereal::BinaryOutputArchive oa(ss);
     oa << packet_;
 
     // set [out] parameter
     std::string& str = ss.str();
-    buffer_.reserve( str.size() );
-    buffer_.assign( str.begin(), str.end() );
+    buffer_.reserve(str.size());
+    buffer_.assign(str.begin(), str.end());
 }//PacketToBuffer()
 
 
 template<typename T>
 void PacketToBuffer( IN T& packet_, OUT std::stringstream& ss_ )
 {
-    boost::archive::text_oarchive oa{ ss_ };
+    cereal::BinaryOutputArchive oa(ss_);
     oa << packet_;
 }//PacketToBuffer()
 
